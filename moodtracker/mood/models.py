73b695ba9cb2824502052728ff2f499ddb70e032
root <root@ip-172-31-0-12.us-west-1.compute.internal>
2017-11-09 18:32:20 +0000

    SMS response support added.  Database user model added

--- moodtracker/mood/models.py
+++ moodtracker/mood/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class MoodUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=9)
--- moodtracker/mood/views.py
+++ moodtracker/mood/views.py
@@ -4,8 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world")
+    return HttpResponse("Hello, world")
+
+def incoming_sms(request):
+    twiml = '<Response><Message>Thanks for your response!</Message></Response>'
+    return HttpResponse(twiml, content_type='text/xml')
